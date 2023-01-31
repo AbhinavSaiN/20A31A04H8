91417f9619ee0a802795a4a358071579fe6da5b8
AbhinavSaiN <113780574+AbhinavSaiN@users.noreply.github.com>
2023-01-31 15:26:27 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,14 @@
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print("\n")
+
+
+a=1
+while(a<=6):
+    b=1
+    while(b<=a):
+        print(b,end=" ")
+        b+=1
+    print("\n")
+    a+=1
