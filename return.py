487af01199ffda7feac8d1c117a36a8253021360
AbhinavSaiN <113780574+AbhinavSaiN@users.noreply.github.com>
2023-02-04 09:33:55 +0530

    Add files via upload

---
+++ return.py
@@ -0,0 +1,6 @@
+# write a program with a user defined function which returns a integer value to the cube
+def cube(x):
+    return(x*x*x)
+num=10
+result=cube(num)
+print("output of the evaluation is",result)
---
+++ value.py
@@ -0,0 +1,12 @@
+# write a program to add six user defined numbers and return to main program
+
+def summ(a,b,c,d,e,f):
+    return(a+b+c+d+e+f)
+a=int(input('enter value'))
+b=int(input('enter value'))
+c=int(input('enter value'))
+d=int(input('enter value'))
+e=int(input('enter value'))
+f=int(input('enter value'))
+result=summ(a,b,c,d,e,f)
+print("output of the evaluation is",result)
